13d7e767586c8244c7e3fb2b87db0217c190610f
Juan Felipe Rada <j.f.r.a1997@gmail.com>
2023-02-08 01:18:25 -0300

    feat: create base Strapi project and config Cloudinary

---
+++ config/plugins.js
@@ -0,0 +1,30 @@
+/**
+ * RADAPLS PROJECTS
+ * ------------------
+ * Copyright (C) 2023 Juan Felipe Rada - All Rights Reserved.
+ *
+ * This file, project or its parts can not be copied and/or distributed without
+ * the express permission of Juan Felipe Rada.
+ *
+ * @file plugins.js
+ * @author Juan Felipe Rada <radapls8@gmail.com>
+ * @date Wednesday, 8th February 2023
+ */
+
+module.exports = ({ env }) => ({
+    upload: {
+        config: {
+            provider: 'cloudinary',
+            providerOptions: {
+                cloud_name: env('CLOUDINARY_NAME'),
+                api_key: env('CLOUDINARY_KEY'),
+                api_secret: env('CLOUDINARY_SECRET'),
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
+            },
+        },
+    },
+});
